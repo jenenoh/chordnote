bba8969986e8e47a46a46db0e04154f3c97abfdb
JennyNoh <150084357+jenjen-02@users.noreply.github.com>
2025-03-18 00:11:10 -0700

    Update layout

--- src/navigation.ts
+++ src/navigation.ts
@@ -1,114 +1,22 @@
-import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
+import { getHomePermalink, getPermalink, getAsset } from './utils/permalinks';
 
 export const headerData = {
   links: [
     {
       text: 'Home',
-      links: [
-        {
-          text: 'SaaS',
-          href: getPermalink('/homes/saas'),
-        },
-        {
-          text: 'Startup',
-          href: getPermalink('/homes/startup'),
-        },
-        {
-          text: 'Mobile App',
-          href: getPermalink('/homes/mobile-app'),
-        },
-        {
-          text: 'Personal',
-          href: getPermalink('/homes/personal'),
-        },
-      ],
+      href: getHomePermalink()
     },
     {
-      text: 'Pages',
-      links: [
-        {
-          text: 'Features (Anchor Link)',
-          href: getPermalink('/#features'),
-        },
-        {
-          text: 'Services',
-          href: getPermalink('/services'),
-        },
-        {
-          text: 'Pricing',
-          href: getPermalink('/pricing'),
-        },
-        {
-          text: 'About us',
-          href: getPermalink('/about'),
-        },
-        {
-          text: 'Contact',
-          href: getPermalink('/contact'),
-        },
-        {
-          text: 'Terms',
-          href: getPermalink('/terms'),
-        },
-        {
-          text: 'Privacy policy',
-          href: getPermalink('/privacy'),
-        },
-      ],
+      text: 'About',
+      href: getPermalink('/about'),
     },
     {
-      text: 'Landing',
-      links: [
-        {
-          text: 'Lead Generation',
-          href: getPermalink('/landing/lead-generation'),
-        },
-        {
-          text: 'Long-form Sales',
-          href: getPermalink('/landing/sales'),
-        },
-        {
-          text: 'Click-Through',
-          href: getPermalink('/landing/click-through'),
-        },
-        {
-          text: 'Product Details (or Services)',
-          href: getPermalink('/landing/product'),
-        },
-        {
-          text: 'Coming Soon or Pre-Launch',
-          href: getPermalink('/landing/pre-launch'),
-        },
-        {
-          text: 'Subscription',
-          href: getPermalink('/landing/subscription'),
-        },
-      ],
+      text: 'Meet the Team',
+      href: getPermalink('meet-the-team'),
     },
     {
-      text: 'Blog',
-      links: [
-        {
-          text: 'Blog List',
-          href: getBlogPermalink(),
-        },
-        {
-          text: 'Article',
-          href: getPermalink('get-started-website-with-astro-tailwind-css', 'post'),
-        },
-        {
-          text: 'Article (with MDX)',
-          href: getPermalink('markdown-elements-demo-post', 'post'),
-        },
-        {
-          text: 'Category Page',
-          href: getPermalink('tutorials', 'category'),
-        },
-        {
-          text: 'Tag Page',
-          href: getPermalink('astro', 'tag'),
-        },
-      ],
+      text: 'Try Out Chordnote',
+      href: getPermalink('try-out-chordnote'),
     },
   ],
 };
@@ -118,10 +26,10 @@ export const footerData = {
     {
       title: 'Resources',
       links: [
-        { text: 'About', href: '#' },
-        { text: 'Meet the Team', href: '#' },
-        { text: 'Try Out Chordnote', href: '#' },
-        { text: 'Download', href: '#' },
+        { text: 'About', href: '/about' },
+        { text: 'Meet the Team', href: 'meet-the-team' },
+        { text: 'Try Out Chordnote', href: 'try-out-chordnote' },
+        { text: 'Download', href: getHomePermalink() + '#download' },
       ],
     },
   ],
